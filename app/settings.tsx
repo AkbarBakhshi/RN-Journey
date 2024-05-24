e0b09898fd3809f4429834250a64e5165748bbd1
Akbar Bakhshi Zanjani <akbzanj@gmail.com>
2024-05-24 14:37:07 -0400

    Updated text color for drawer tabs

--- app/settings.tsx
+++ app/settings.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Settings() {
   return (
     <View className="flex flex-1 items-center justify-center">
-      <Text>Drawer [Settings]</Text>
+      <Text className='text-foreground'>Drawer [Settings]</Text>
     </View>
   );
 }
--- app/shop/_layout.tsx
+++ app/shop/_layout.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Shop() {
   return (
     <View className="flex flex-row flex-1 items-center justify-center">
-      <Text>Drawer [Shop]</Text>
+      <Text className="text-foreground">Drawer [Shop]</Text>
     </View>
   );
 }
